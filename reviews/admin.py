7d496b2ebcae8d49b800785b5c68cb3b016eb847
X2X <vitenikpavel@gmail.com>
2026-06-12 19:00:17 +0300

    fдобавил Интеграцию с административной панелью

--- reviews/admin.py
+++ reviews/admin.py
@@ -3,8 +3,8 @@ from .models import Review
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):  
-    list_display = ('title', 'id', 'is_published', 'created_at', 'updated_at')
-    list_filter = ('is_published', 'created_at')
+    list_display = ('title', 'id', 'is_published', 'created_at', 'updated_at', 'author')
+    list_filter = ('is_published', 'created_at', 'author')
     search_fields = ('title', 'content')
     readonly_fields = ('id', 'created_at', 'updated_at')
     prepopulated_fields = {'slug': ('title',)}
