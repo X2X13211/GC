87aefa280b00d098cb763b165e61c88df96c0fe6
X2X <vitenikpavel@gmail.com>
2026-06-16 20:11:33 +0300

    Интегрировал View, рендеринг UI и устраненил  уязвимости N+1

--- config/urls.py
+++ config/urls.py
@@ -15,14 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.views.generic import TemplateView
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('', include('reviews.urls')),
 ]
 
 if settings.DEBUG:
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Review
+
 
 # Create your views here.
+# 2.1. Слой контроллеров (reviews/views.py)
+
+# Необходимо разработать функцию-контроллер для обработки запросов к главной странице ленты.
+
+# Фильтрация: Контроллер должен извлекать из БД только те обзоры, у которых статус публикации имеет значение “Истина” (опубликованы).
+
+# Оптимизация (Критическое требование): Примените метод ORM select_related для присоединения данных автора (таблица User) к выборке обзоров в рамках единого SQL-запроса (INNER JOIN).
+
+# Передача данных: Сформируйте контекст (словарь) и передайте извлеченный QuerySet в шаблон.
+
+def review_list(request): 
+    reviews = Review.objects.filter(is_published = True).select_related('author')
+    context = {'reviews': reviews}
+    return render(request, 'reviews/review_list.html', context)
